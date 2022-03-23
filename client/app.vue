f10366d5fd4ce38942efefd208583587f4909c7f
LouisEiermann <louis.eiermann@gmail.com>
2022-03-23 22:45:44 +0100

    basic chat function

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<input v-model="text" />
+		<button @click="post()">Post</button>
+		<div v-for="message in messages" v-bind:key="message">
+			{{ message }}
+		</div>
+	</div>
 </template>
+<script setup>
+import Gun from "gun";
+import { ref } from "vue";
+
+let gun = Gun(["localhost:3000"]);
+
+let copy = gun.get("test").get("paste");
+
+let messages = [];
+
+const text = ref("");
+
+function post() {
+	console.log(text.value);
+	copy.put(text.value);
+}
+
+copy.on((data) => {
+	messages.push(data);
+});
+</script>
